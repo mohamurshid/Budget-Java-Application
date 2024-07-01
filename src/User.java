50fe4f13921722b400bb45bd83e4fc28b2b79bd3
moham <mohamurshid153@gmail.com>
2024-07-01 14:18:58 +0300

    First Commit

---
+++ src/User.java
@@ -0,0 +1,24 @@
+public class User {
+    private String username;
+    private String password; // Note: In a real app, passwords MUST be hashed
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                '}';
+    }
+}
